f462c850aadb37573d8110e1c28fc986fd6f1fa2
John Victor-Faichney <john.victor.faichney@gmail.com>
2014-12-29 20:11:43 -0800

    loading project info from YAML file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
  * Module dependencies
  */
 var express = require('express') , 
+  YAML = require('yamljs'),
   morgan = require( 'morgan')
   
 var app = express()
@@ -10,8 +11,16 @@ app.set('views', __dirname + '/client/views')
 app.set('view engine', 'jade')
 app.use( express.static(__dirname + '/client') )
 
+function get_info ( file ) {
+  info = YAML.load( file )
+  console.log( "\n "+file+" \n")
+  console.log( JSON.stringify(info, undefined, 2) );
+  return info
+}
+
 app.get('/', function (req, res) {
   res.render('carousel', { 
+    projectData : get_info( 'info.yml' ),
     pathToAssets :  '/bower_components/bootstrap', 
     pathToImages : '/img',
     pathToSelectedTemplateWithinBootstrap : '/stylesheets'
