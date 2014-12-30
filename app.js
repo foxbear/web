5f568de0a74de331aeb9b6296c491bfd3f7d432e
John Victor-Faichney <john.victor.faichney@gmail.com>
2014-12-29 16:48:27 -0800

    got carousel template working

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
 /*
  * Module dependencies
  */
-var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
-  , morgan = require( 'morgan')
+var express = require('express') , 
+  morgan = require( 'morgan')
   
 var app = express()
 
-function compile( str, path ) {
-    return stylus(str)
-    .set('filename', path)
-    .use(nib())
-}
-
 app.set('views', __dirname + '/client/views')
 app.set('view engine', 'jade')
-app.use( morgan('combined') )
-app.use( stylus.middleware( { 
-    src: __dirname + '/client',
-    compile: compile
-    }
-))
 app.use( express.static(__dirname + '/client') )
 
 app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
+  res.render('carousel', { 
+    // pathToAssets : __dirname + '/client/bower_components/bootstrap', 
+    // pathToSelectedTemplateWithinBootstrap :  __dirname + '/client/stylesheets'
+    pathToAssets :  '/bower_components/bootstrap', 
+    pathToImages : '/img'
+    pathToSelectedTemplateWithinBootstrap : '/stylesheets'
+    }
   )
 })
 
 app.listen(3000)
+console.log( "Serving app.js at localhost:3000...")
